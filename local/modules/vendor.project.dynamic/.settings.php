9389a3688d6c24a551cdd09e42de4b6929c1e790
Алексей Назаров <AlSNazarov@1cbit.ru>
2022-12-17 00:56:40 +0500

    update custom section module

--- local/modules/vendor.project.dynamic/.settings.php
+++ local/modules/vendor.project.dynamic/.settings.php
@@ -1,10 +1,17 @@
 <?php
 
+use Vendor\Project\Dynamic\Service\Integration\Intranet\CustomSectionProvider;
+
 return [
     'controllers' => [
         'value' => [
             'defaultNamespace' => '\\Vendor\\Project\\Dynamic\\Controller',
         ],
         'readonly' => true,
+    ],
+    'intranet.customSection' => [
+        'value' => [
+            'provider' => CustomSectionProvider::class,
+        ],
     ],
 ];
